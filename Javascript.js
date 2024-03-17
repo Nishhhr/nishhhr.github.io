9432cb260f4ded7749ca5b385693b0670b92ba3e
Nishhhr <157746451+Nishhhr@users.noreply.github.com>
2024-03-16 21:32:19 -0400

    main website

--- Javascript.js
+++ Javascript.js
@@ -14,3 +14,15 @@ function resetForm() {
     document.getElementById("myForm").reset();
     
 }
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "100px";
+    document.body.style.backgroundColor = "#f6f1f2";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "#f6f1f2";
+  }
