13768eabd38b598fc2fafbf46816065a95ba233e
Nishhhr <157746451+Nishhhr@users.noreply.github.com>
2024-03-18 18:59:40 -0400

    new

--- Javascript.js
+++ Javascript.js
@@ -1,19 +1,3 @@
-function myFunction(){
-    onclick=prompt ('what is your email');
-    }
-    
-function enlarge(){
-    onclick= width="570"; height="500"
-}
-
-localStorage.setItem("fname", "marijn"); 
-console.log(localStorage.getItem("fname"));
-localStorage.removeItem("fname"); 
-
-function resetForm() {
-    document.getElementById("myForm").reset();
-    
-}
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
