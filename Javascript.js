238e6f9e72b06993f5b264852feee1dee6973651
Nishhhr <157746451+Nishhhr@users.noreply.github.com>
2024-03-25 23:34:09 -0400

    lalal

--- Javascript.js
+++ Javascript.js
@@ -10,3 +10,13 @@ function openNav() {
     document.getElementById("main").style.marginLeft= "0";
     document.body.style.backgroundColor = "#f6f1f2";
   }
+
+  function changeFontSize(size) {
+    document.querySelector('p').style.fontSize = size;
+  }
+
+  function toggleColorblindMode() {
+    document.getElementById("main").classList.toggle('colorblind-mode');
+  }
+
+  document.getElementById('colorblind-toggle').addEventListener('click', toggleColorblindMode);
